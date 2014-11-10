889cf98eca7ac8f69fc5fcd143da3ddb42281e7b
John Marks <john@bluefroggaming.com>
2014-11-10 16:49:29 -0500

    add acts_as_rateable to activerecord

--- lib/multiplayer_elo.rb
+++ lib/multiplayer_elo.rb
@@ -3,6 +3,7 @@ require 'multiplayer_elo/rating_manager'
 
 module MultiplayerElo
   module Rateable
+    extend ActiveSupport::Concern
 
     module ClassMethods
       def acts_as_rateable(options = {})
@@ -11,3 +12,5 @@ module MultiplayerElo
     end
   end
 end
+
+ActiveRecord::Base.send(:include, MultiplayerElo::Rateable)
--- spec/acts_as_rateable_spec.rb
+++ spec/acts_as_rateable_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe MultiplayerElo do
   it 'adding acts_as_rateable should be included in active record' do
-    expect do
-      class Foo < ActiveRecord::Base
-        acts_as_rateable
-      end
+    class Foo < ActiveRecord::Base
+      acts_as_rateable
     end
   end
+
 end
